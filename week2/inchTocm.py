2eb10a2e7c2ab8f98965458209e738ea8bc7ede2
SooHwanWoo <163228559+SooHwanWoo@users.noreply.github.com>
2026-03-11 22:47:55 +0900

    Delete week2 directory

--- week2/inchTocm.py
+++
@@ -1,7 +0,0 @@
-#1.inch 단위를 입력받아 cm 단위 구하는 코드를 작성하세요 (inchTocm.py)
-
-inch = float(input("inch 값: "))
-
-cm = inch * 2.54
-
-print("변환 결과:", cm, "cm")
--- week2/kgToPound.py
+++
@@ -1,7 +0,0 @@
-# 킬로그램 단위 입력받아 파운드 단위 구하는 코드를 작성하세요(kgToPound.py)
-
-kg = float(input("kg 값: "))
-
-pound = kg * 2.20462
-
-print("pound: ", pound)
